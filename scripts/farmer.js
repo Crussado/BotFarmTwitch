b2c29ff658b9001ab5be6525124b5018c90cfb29
Julio Olivencia <julioolivencia891@gmail.com>
2021-11-07 14:41:20 -0300

    se crea el script de farm

---
+++ scripts/farmer.js
@@ -0,0 +1,39 @@
+const TIME_BUTTON = 100000;
+
+function getButton() {
+    return document.querySelectorAll('button.ScCoreButton-sc-1qn4ixc-0.ScCoreButtonSuccess-sc-1qn4ixc-5');
+}
+
+function farm() {
+    setTimeout(() => {
+        getButton().click();
+        farm();
+    }, TIME_BUTTON);
+}
+
+
+function main () {
+    farm();
+}
+
+// $(document).ready(function() {
+    
+//     llamar_ajax(); // al cargar el documento, se ejecutará dicha función
+
+//     function llamar_ajax(){  // encapsular función ajax
+//     var user_id = $("#mi_selector").attr('empresa_id'); // debes obtener el selector directo
+//        $.ajax({
+//        url: "{{url('admin/companies/editaddress')}}",
+//        type: "POST",
+//        data: '&empresa_id='+user_id,
+//        success: function (data) {
+//           $('.editContent').html(data);
+//           $('#editAddressModal').modal('show');
+//        },
+//        dataType: 'html'
+//     });
+//     } // fin función
+// });
+// }
+
+main();
